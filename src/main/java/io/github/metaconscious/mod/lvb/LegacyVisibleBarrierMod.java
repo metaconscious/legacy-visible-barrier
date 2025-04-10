172df6ea68ae217179f961f04275cc1469403e5a
Mark Oven <justmarkoven@gmail.com>
2025-04-10 15:00:40 +0800

    Define mod constants for ID, name, and version in LegacyVisibleBarrierMod class

--- src/main/java/io/github/metaconscious/mod/lvb/LegacyVisibleBarrierMod.java
+++ src/main/java/io/github/metaconscious/mod/lvb/LegacyVisibleBarrierMod.java
@@ -3,12 +3,17 @@ package io.github.metaconscious.mod.lvb;
 import net.fabricmc.api.ModInitializer;
 
 public class LegacyVisibleBarrierMod implements ModInitializer {
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
-	}
+    public static final String MOD_ID = "lvb";
+    public static final String MOD_NAME = "Legacy Visible Barrier";
+    public static final String MOD_VERSION = "1.0";
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        System.out.println("Hello Fabric world!");
+    }
 }
