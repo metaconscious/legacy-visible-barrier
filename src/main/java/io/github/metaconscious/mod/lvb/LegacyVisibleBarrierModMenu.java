4e5130549aa30e768968d897a7aac522a2cea8aa
Mark Oven <justmarkoven@gmail.com>
2025-04-13 13:42:32 +0800

    Add configuration screen to mod menu via ModMenu API integration

--- src/main/java/io/github/metaconscious/mod/lvb/LegacyVisibleBarrierModMenu.java
+++ src/main/java/io/github/metaconscious/mod/lvb/LegacyVisibleBarrierModMenu.java
@@ -1,7 +1,24 @@
 package io.github.metaconscious.mod.lvb;
 
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
+import io.github.prospector.modmenu.gui.AbstractScreen;
+import net.minecraft.client.gui.screen.Screen;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LegacyVisibleBarrierModMenu implements ModMenuApi {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ModMenuConfigScreen::new;
+    }
+
+    private static final class ModMenuConfigScreen extends AbstractScreen {
+        public ModMenuConfigScreen(Screen previous) {
+            super(previous);
+        }
+    }
 }
